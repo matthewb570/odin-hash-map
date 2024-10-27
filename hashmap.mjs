2f967571fd66d7659cf7866bc6b919757989dde3
Matthew Burgess <boog397@gmail.com>
2024-10-26 22:43:16 -0500

    Add HashMap class

---
+++ hashmap.mjs
@@ -0,0 +1,119 @@
+import LinkedList from "./linkedlist.mjs";
+
+// TODO: Add growing functionality
+// TODO: Test
+
+class HashMap {
+
+    buckets;
+
+    constructor() {
+        buckets = new Array(16);
+    }
+
+    hash(key) {
+        let hashCode = 0;
+      
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i) % buckets.length;
+        }
+
+        return hashCode;
+    }
+
+    set(key, value) {
+        const keyHash = this.hash(key);
+
+        if (this.buckets[keyHash] === undefined || this.buckets[keyHash] === null) {
+            this.buckets[keyHash] = new LinkedList();
+        } else if (this.buckets[keyHash].contains(key)) {
+            this.buckets[keyHash].removeAt(this.buckets[keyHash].find(key));
+        }
+        this.buckets[keyHash].append(key, value);
+    }
+
+    get(key) {
+        const keyHash = this.hash(key);
+
+        if (this.buckets[keyHash] !== undefined && this.buckets[keyHash] !== null) {
+            const linkedListIndex = this.buckets[keyHash].find(key);
+            if (linkedListIndex !== null) {
+                return this.buckets[keyHash].at(linkedListIndex).value;
+            }
+        }
+
+        return null;
+    }
+
+    has(key) {
+        return this.get(key) !== null ? true : false;
+    }
+
+    remove(key) {
+        if (!this.has(key)) {
+            return false;
+        }
+
+        const keyHash = this.hash(key);
+        const linkedListIndex = this.buckets[keyHash].find(key);
+        this.buckets[keyHash].removeAt(linkedListIndex);
+        return true;
+    }
+
+    length() {
+        let length = 0;
+        this.buckets.forEach((linkedList) => {
+            if (linkedList !== undefined && linkedList !== null) {
+                length += linkedList.size;
+            }
+        });
+        return length;
+    }
+
+    clear() {
+        this.buckets = new Array(16);
+    }
+
+    keys() {
+        let keysList = [];
+        this.buckets.forEach((linkedList) => {
+            if (linkedList !== undefined && linkedList !== null) {
+                for (let i = 0; i < linkedList.size(); i++) {
+                    keysList.push(linkedList.at(i).key);
+                }
+            }
+        });
+        return keysList;
+    }
+
+    values() {
+        let valuesList = [];
+        this.buckets.forEach((linkedList) => {
+            if (linkedList !== undefined && linkedList !== null) {
+                for (let i = 0; i < linkedList.size(); i++) {
+                    valuesList.push(linkedList.at(i).value);
+                }
+            }
+        });
+        return valuesList;
+    }
+
+    entries() {
+        let entriesList = [];
+        this.buckets.forEach((linkedList) => {
+            if (linkedList !== undefined && linkedList !== null) {
+                for (let i = 0; i < linkedList.size(); i++) {
+                    let node = linkedList.at(i);
+                    entriesList.push({
+                        key: node.key,
+                        value: node.value
+                    });
+                }
+            }
+        });
+        return entriesList;
+    }
+}
+
+export default HashMap;
