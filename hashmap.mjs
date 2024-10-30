611f24fc85b7403ec4e34ae9cc1cb5dff950fbcc
Matthew Burgess <boog397@gmail.com>
2024-10-29 22:58:33 -0500

    Fix bugs

--- hashmap.mjs
+++ hashmap.mjs
@@ -1,8 +1,5 @@
 import LinkedList from "./linkedlist.mjs";
 
-// TODO: Add growing functionality
-// TODO: Test
-
 const LOAD_FACTOR = 0.75;
 
 class HashMap {
@@ -10,7 +7,7 @@ class HashMap {
     buckets;
 
     constructor() {
-        buckets = new Array(16);
+        this.buckets = new Array(16);
     }
 
     hash(key) {
@@ -18,18 +15,18 @@ class HashMap {
       
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
-            hashCode = primeNumber * hashCode + key.charCodeAt(i) % this.buckets.length;
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.buckets.length;
         }
 
         return hashCode;
     }
 
     set(key, value) {
-        if (this.size() >= this.buckets.length * LOAD_FACTOR) {
+        const keyHash = this.hash(key);
+
+        if ((this.buckets[keyHash] === undefined || this.buckets[keyHash] === null || !this.buckets[keyHash].contains(key)) && this.length() >= this.buckets.length * LOAD_FACTOR) {
             this.grow();
         }
-        
-        const keyHash = this.hash(key);
 
         if (this.buckets[keyHash] === undefined || this.buckets[keyHash] === null) {
             this.buckets[keyHash] = new LinkedList();
@@ -71,7 +68,7 @@ class HashMap {
         let length = 0;
         this.buckets.forEach((linkedList) => {
             if (linkedList !== undefined && linkedList !== null) {
-                length += linkedList.size;
+                length += linkedList.size();
             }
         });
         return length;
@@ -122,7 +119,7 @@ class HashMap {
     }
 
     grow() {
-        const entriesList = entries();
+        const entriesList = this.entries();
         this.buckets = new Array(this.buckets.length * 2);
         entriesList.forEach(entry => {
             this.set(entry.key, entry.value);
