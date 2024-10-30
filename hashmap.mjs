e0a06e3ac18a11fb62e25f5001731d7ec52b1677
Matthew Burgess <boog397@gmail.com>
2024-10-29 21:25:29 -0500

    Add growth logic to HashMap

--- hashmap.mjs
+++ hashmap.mjs
@@ -3,6 +3,8 @@ import LinkedList from "./linkedlist.mjs";
 // TODO: Add growing functionality
 // TODO: Test
 
+const LOAD_FACTOR = 0.75;
+
 class HashMap {
 
     buckets;
@@ -16,13 +18,17 @@ class HashMap {
       
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
-            hashCode = primeNumber * hashCode + key.charCodeAt(i) % buckets.length;
+            hashCode = primeNumber * hashCode + key.charCodeAt(i) % this.buckets.length;
         }
 
         return hashCode;
     }
 
     set(key, value) {
+        if (this.size() >= this.buckets.length * LOAD_FACTOR) {
+            this.grow();
+        }
+        
         const keyHash = this.hash(key);
 
         if (this.buckets[keyHash] === undefined || this.buckets[keyHash] === null) {
@@ -113,6 +119,14 @@ class HashMap {
             }
         });
         return entriesList;
+    }
+
+    grow() {
+        const entriesList = entries();
+        this.buckets = new Array(this.buckets.length * 2);
+        entriesList.forEach(entry => {
+            this.set(entry.key, entry.value);
+        });
     }
 }
 
